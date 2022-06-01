302c3f5aeed25a772a2635b4d32394151f58ccca
Luizrodgs <luizrodgs1@gmail.com>
2022-06-01 19:35:20 -0300

    Cria zip 'files.zip'

--- Challenge01/challenge01.py
+++ Challenge01/challenge01.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import zipfile
+import sys
 
 
 def download_file(url, file_address):
@@ -12,9 +14,29 @@ def download_file(url, file_address):
         response.raise_for_status()
 
 
-if __name__ == '__main__':
-    url_file1 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537.pdf'
-    output_dir = 'Downloads'
+def zip_files():
+    zip = zipfile.ZipFile('files.zip', 'w', zipfile.ZIP_DEFLATED)
+    # zip.write()
+    # zip.close()
 
-    file_name = os.path.join(output_dir, 'anexo.pdf')
-    download_file(url_file1, file_name)
+
+url_file1 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537.pdf'
+url_file2 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537.xlsx'
+url_file3 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_II_DUT_2021_RN_465.2021_tea.br_RN473_RN477_RN478_RN480_RN513_RN536.pdf'
+url_file4 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_III_DC_2021_RN_465.2021.v2.pdf'
+url_file5 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_IV_PROUT_2021_RN_465.2021.v2.pdf'
+url_list = [url_file1, url_file2, url_file3, url_file4, url_file5]
+
+
+if __name__ == '__main__':
+    output_dir = 'Output'
+    c = 0
+    for url in url_list:
+        c += 1
+        if url.endswith('.pdf'):
+            file_name = os.path.join(output_dir, 'anexo{}.pdf'.format(c))
+            download_file(url, file_name)
+        elif url.endswith('.xlsx'):
+            file_name = os.path.join(output_dir, 'anexo{}.xlsx'.format(c))
+            download_file(url, file_name)
+    zip_files()
