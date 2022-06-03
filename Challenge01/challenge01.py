962a3aef68cc97e0d07ce962dc777cab25f9a9d7
Luizrodgs <luizrodgs1@gmail.com>
2022-06-02 23:03:48 -0300

    Coleta URLs para download dos anexos e refatoração

--- Challenge01/challenge01.py
+++ Challenge01/challenge01.py
@@ -1,46 +1,52 @@
 import os
-import requests
+import re
 from shutil import make_archive
 
-def main(url_list, c, output_dir, download_file, zip_files):
-    for url in url_list:
-        c += 1
-        if url.endswith('.pdf'):
-            file_name = os.path.join(output_dir, 'anexo{}.pdf'.format(c))
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    for c, url in enumerate(url_list):
+        if url.endswith(".pdf"):
+            file_name = os.path.join(output_dir, "anexo{}.pdf".format(c + 1))
             download_file(url, file_name)
-        elif url.endswith('.xlsx'):
-            file_name = os.path.join(output_dir, 'anexo{}.xlsx'.format(c))
+        elif url.endswith(".xlsx"):
+            file_name = os.path.join(output_dir, "anexo{}.xlsx".format(c + 1))
             download_file(url, file_name)
     zip_files()
 
 
-# Faz o download do doc referenciado
 def download_file(url, file_address):
+    """Faz o download do doc referenciado"""
     response = requests.get(url)
     if response.status_code == requests.codes.OK:
-        with open(file_address, 'wb') as new_file:
+        with open(file_address, "wb") as new_file:
             new_file.write(response.content)
-        print('Download completed. File saved: {}'.format(file_address))
+        print("Download completed. File saved: {}".format(file_address))
     else:
         response.raise_for_status()
 
 
-# Zipa a pasta Output com os docs baixados
 def zip_files():
-    make_archive('Files-Challenge01', 'zip', 'Output')
-    print('Zip completed')
+    """Zipa a pasta Output com os docs baixados"""
+    make_archive("Files-Challenge01", "zip", "Output")
+    print("Zip completed")
 
 
-url_file1 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537.pdf'
-url_file2 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_I_Rol_2021RN_465.2021_RN473_RN478_RN480_RN513_RN536_RN537.xlsx'
-url_file3 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_II_DUT_2021_RN_465.2021_tea.br_RN473_RN477_RN478_RN480_RN513_RN536.pdf'
-url_file4 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_III_DC_2021_RN_465.2021.v2.pdf'
-url_file5 = 'https://www.gov.br/ans/pt-br/arquivos/assuntos/consumidor/o-que-seu-plano-deve-cobrir/Anexo_IV_PROUT_2021_RN_465.2021.v2.pdf'
-url_list = [url_file1, url_file2, url_file3, url_file4, url_file5]
+def collect_urls():
+    base_response = requests.get(
+        "https://www.gov.br/ans/pt-br/assuntos/consumidor/o-que-o-seu-plano-de-saude-deve-cobrir-1/o-que-e-o-rol-de-procedimentos-e-evento-em-saude"
+    )
+    base_content = base_response.content
+    soup = BeautifulSoup(base_content, "html.parser")
+    url_list = []
+    for file_url in soup.find_all("a", string=re.compile("Anexo")):
+        url_list.append(file_url["href"])
+    return url_list
 
 
-if __name__ == '__main__':
-    output_dir = 'Output'
-    c = 0
-    main(url_list, c, output_dir, download_file, zip_files)
-
+if __name__ == "__main__":
+    output_dir = "Output"
+    url_list = collect_urls()
+    main()
